85cdad482e9250b5e4cf46f9d028b4bbda9d3ac3
David Wildasin <swdragonmaus@gmail.com>
2017-10-26 21:17:34 -0400

    general: Make package list update module more robust

--- etc/update.d/13-package-list.sh
+++ etc/update.d/13-package-list.sh
@@ -1,13 +1,7 @@
-#!/bin/sh
-
 grpfile=$HOME/etc/pacman/groups
 pkgfile=$HOME/etc/pacman/packages
-
-groups=`cat "$grpfile"`
-grouped=`pacaur -Qgq $groups | sort -u`
-
-pacaur -D --asdeps $grouped >/dev/null 2>&1
-
+xargs pacaur -Qgq <"$grpfile" | sort -u | xargs pacaur -D --asdeps 1>/dev/null 2>&1
+rm -f "$pkgfile"{new}
 pacaur -Qeq | sort -u >"$pkgfile"{new}
 fsync "$pkgfile"{new}
 if cmp -s "$pkgfile"{new} "$pkgfile"; then
@@ -15,5 +9,4 @@ if cmp -s "$pkgfile"{new} "$pkgfile"; then
 else
   mv -f "$pkgfile"{new} "$pkgfile"
 fi
-
-pacaur -D --asexplicit $grouped >/dev/null 2>&1
+xargs pacaur -Qgq <"$grpfile" | sort -u | xargs pacaur -D --asexplicit 1>/dev/null 2>&1
